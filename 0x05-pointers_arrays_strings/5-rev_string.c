976c54244ddf159b5eeb3ee5b3428e0c6c69b8fe
solomon1227 <solomonf1227@gmail.com>
2022-09-19 21:40:37 +0300

    reverse the string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,13 +6,14 @@
  */
 void rev_string(char *s)
 {
-	char *reversed;
+	char *tmp;
 	int i;
 	int j;
 
-	*reversed = *s;
-	for (i = strlen(reversed) - 1, j = 0; i >= 0; i--, j++)
+	tmp = s;
+	for (i = strlen(s) - 1, j = 0; i >= 0; i--, j++)
 	{
-		*(s + j) = *(reversed + i);
+		*(tmp + j) = *(s + i);
 	}
+	s = tmp;
 }
