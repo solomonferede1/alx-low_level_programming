0a0103bd63b1694f9fca2f303714374ced3b4619
solomon1227 <solomonf1227@gmail.com>
2022-09-22 17:01:49 +0300

    string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strcmp - compare two string
+ * @s1: string to be compared
+ * @s2: string to be compared
+ * Return: 0 - if match, less than 0 if s1 less than s2, > 0 if s1 greater s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
