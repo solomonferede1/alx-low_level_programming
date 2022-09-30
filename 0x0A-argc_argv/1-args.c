cdda643294b3c16f68ff5e2ed32911683fc64a90
solomon1227 <solomonf1227@gmail.com>
2022-09-30 23:04:45 +0300

    print number of arguments to main

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+/**
+ * main - print name of the program
+ *
+ * @argc: number of arguments to main
+ * @argv: arguments of main
+ *
+ * Return: always 0
+ */
+
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
