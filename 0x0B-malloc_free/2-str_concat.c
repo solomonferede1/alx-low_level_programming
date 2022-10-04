7403e23f4da7ff9cb6073a68e49cc13c1a95670f
solomon1227 <solomonf1227@gmail.com>
2022-10-04 09:58:05 +0300

    Concatenate string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,24 +16,22 @@ char *str_concat(char *s1, char *s2)
 	int len;
 	int len1, len2;
 
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	len = len1 + len2;
-
-	if (s1 == NULL && s2 != NULL)
-		return (s2);
-	if (s1 != NULL && s2 == NULL)
-		return (s1);
-	if (s1 == NULL && s2 == NULL)
-		return ("");
 
 	s1_s2 = malloc(sizeof(char) * (len + 1));
 
 	if (s1_s2 == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i]; i++)
 		s1_s2[i] = s1[i];
-	for (i = 0; s2[i] != '\0'; i++)
+	for (i = 0; s2[i]; i++)
 		s1_s2[i + len1] = s2[i];
 	return (s1_s2);
 }
