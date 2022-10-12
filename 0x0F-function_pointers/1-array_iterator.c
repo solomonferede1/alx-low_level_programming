a2d07fa061f2c33a65fb7d1ba857520fbea74a12
solomon1227 <solomonf1227@gmail.com>
2022-10-12 16:57:46 +0300

    xecutes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (!array || !action)
-		exit(EXIT_FAILURE);
+		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
