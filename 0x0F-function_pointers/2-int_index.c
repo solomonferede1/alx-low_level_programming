aefe3e69f8efc15d1f25349bd29d80045ee90c9b
solomon1227 <solomonf1227@gmail.com>
2022-10-12 17:10:35 +0300

    searches an integer in an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (!array || !cmp)
-		exit(EXIT_FAILURE);
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
