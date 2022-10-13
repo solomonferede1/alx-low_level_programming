9f736c52a74cd0c71c86d1b9bb32d0332464c60d
solomon1227 <solomonf1227@gmail.com>
2022-10-13 22:12:14 +0300

    the main function is added

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+/**
+  * main - main
+  * @argc: number of arguments to main
+  * @argv: array of argunments
+  * Return: 0 on success
+  */
+
+int main(int argc, char **argv)
+{
+	int result;
+	int (*funn)(int, int);
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	funn = get_op_func(argv[2]);
+	if (funn == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	result = funn(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
