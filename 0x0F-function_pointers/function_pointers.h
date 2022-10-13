ac69b7b2e30f41369d328b67b6403a94ff4543b6
solomon1227 <solomonf1227@gmail.com>
2022-10-13 22:20:35 +0300

    update header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
