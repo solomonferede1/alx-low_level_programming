9c9858eae0065ba5d44e0285a238f5445c61d730
solomon1227 <solomonf1227@gmail.com>
2022-10-16 04:10:07 +0300

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,61 @@
+#include "variadic_functions.h"
+#include <string.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of type of arguments
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	va_list ph;
+	unsigned int len, af_len;
+	char *string;
+	char *af;
+
+	af = "cifs";
+	len = strlen(format);
+	af_len = strlen(af);
+	va_start(ph, format);
+
+	i = 0;
+	while (i < len)
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ph, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(ph, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ph, double));
+				break;
+			case 's':
+			{
+				string = va_arg(ph, char *);
+				if (string == NULL)
+					printf("nil");
+				else
+					printf("%s", string);
+			}
+				break;
+			default:
+				break;
+		}
+		j = 0;
+		while (j < af_len)
+		{
+			if (format[i] == af[j] && i < len - 1)
+			{
+				printf(", ");
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+}
