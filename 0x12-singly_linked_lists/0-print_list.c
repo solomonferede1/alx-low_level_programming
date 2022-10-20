c4de0999cb6d84efb5a60cfbf86791053baec3c0
solomon1227 <solomonf1227@gmail.com>
2022-10-20 14:35:20 +0300

    print elements of singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 /**
- * print_list - print elements of the list
- * @h: linked lists
- * Return: size
- */
+  * print_list - print elements of the list
+  * @h: linked lists
+  * Return: size
+  */
 size_t print_list(const list_t *h)
 {
-        long unsigned int count;
+	unsigned int count;
 
-        count = 0;
-        while (h)
-        {
-                if (h->str == NULL)
-                        printf("[%d] %s\n", h->len,"(nil)");                
-                else
-                        printf("[%d] %s\n", h->len, h->str);
-                count++;
-                h = h->next;
-        }
-        return (count);
+	count = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] %s\n", "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
