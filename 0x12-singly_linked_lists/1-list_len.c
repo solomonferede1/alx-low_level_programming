e7c57d65f28960833e64199d9d38ec4a3fa423eb
solomon1227 <solomonf1227@gmail.com>
2022-10-20 14:41:37 +0300

    number of elements of singly linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,18 @@
 #include "lists.h"
 /**
- * list_len - return size of a linked list
- * @h: linked lists
- * Return: size of linked lists
- */
+  * list_len - return size of a linked list
+  * @h: linked lists
+  * Return: size of linked lists
+  */
 size_t list_len(const list_t *h)
 {
-        size_t count;
-        size_t i;
+	size_t count;
 
-            
+	count = 0;
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
