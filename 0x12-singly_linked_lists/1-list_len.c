a305bad0ec96d7a4b2670b94e505ca72b8aa7b4a
solomon1227 <solomonf1227@gmail.com>
2022-10-20 12:26:24 +0300

    singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * list_len - return size of a linked list
+ * @h: linked lists
+ * Return: size of linked lists
+ */
+size_t list_len(const list_t *h)
+{
+        size_t count;
+        size_t i;
+
+            
+}
