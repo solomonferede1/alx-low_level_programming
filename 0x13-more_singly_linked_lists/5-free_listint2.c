29730c9ace5eefcecd6d59bb139db145969ea3e6
solomon1227 <solomonf1227@gmail.com>
2022-10-26 05:46:57 +0300

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * fre_listint2 - free to NULL
+ * @head - head of linked lists
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t tmp;
+
+	while (*head)
+	{
+		tmp = *head->next;
+		*head = NULL;
+		*head->next = tmp;
+	}
+}
