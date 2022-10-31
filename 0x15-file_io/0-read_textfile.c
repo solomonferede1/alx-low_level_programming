d973715c38ceb209c9d2d2be510879782a6926aa
solomon1227 <solomonf1227@gmail.com>
2022-10-31 17:16:01 +0300

    A function that reads a text file print on stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	buff = malloc(sizeof(char) * letters + 1);
+	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
 	size = read(fd, buff, letters);
