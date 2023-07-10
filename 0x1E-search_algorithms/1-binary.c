99466998f2459b1625088f83d315437ef70f9b37
ubuntu <ubuntu@ubuntu.ethiotech.tech>
2023-07-10 15:46:09 +0300

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -19,22 +19,20 @@ int binary_search(int *array, size_t size, int value)
 	right = size - 1;
 	while (left <= right)
 	{
+		middle = floor((left + right) / 2);
+		printf("Searching in array: %d", array[left]);
+		for (i = left + 1; i <= right; i++)
 		{
-			middle = floor((left + right) / 2);
-			printf("Searching in array: %d", array[left]);
-			for (i = left + 1; i <= right; i++)
-			{
-				printf(", %d", array[i]);
-			}
-			printf("\n");
-			if (array[middle] < value)
-				left = middle + 1;
-			else
-				if (array[middle] > value)
-					right = middle - 1;
-				else
-					return (middle);
+			printf(", %d", array[i]);
 		}
-		return (-1);
+		printf("\n");
+		if (array[middle] < value)
+			left = middle + 1;
+		else
+			if (array[middle] > value)
+				right = middle - 1;
+			else
+				return (middle);
 	}
+	return (-1);
 }
