65f52f50d8862da70e5a837740e943af264f98f8
solomonferede <solomonf1227@gmail.com>
2024-01-31 11:51:34 +0300

    Function to create a header file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
  * hash_table_create - function to Create a hash table.
  * @size: Size of the array.
  *
- * Return: A pointer to the newly created hash table, or NULL if something went wrong.
+ * Return: A pointer to the newly created hash table, or NULL if went wrong.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
